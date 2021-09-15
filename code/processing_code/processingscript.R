b7eee7f9f81956fe3914ee019638cb053f10c4b7
Carter Coleman <cac64024@uga.edu>
2021-09-14 21:37:47 -0400

    Initial data processing - CC

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -13,18 +13,15 @@ library(here) #to set paths
 #note the use of the here() package and not absolute paths
 data_location <- here::here("data","raw_data","Vaccine_Hesitancy_CDC.xlsx")
 
-#load data. 
-#note that for functions that come from specific packages (instead of base R)
-# I often specify both package and function like so
-#package::function() that's not required one could just call the function
-#specifying the package makes it clearer where the function "lives",
-#but it adds typing. You can do it either way.
+#load vaccine hesitancy data. Here is a description of the data set: "[the included data set]shows estimates of COVID-19 vaccine hesitancy rates using data from the U.S. Census Bureau's Household Pulse Survey (HPS). We estimate hesitancy rates in two steps. First, we estimate hesitancy rates at the state level using the HPS for the collection period May 26, 2021 - June 7, 2021, which is referred to as Week 31. Then, we utilize the estimated values to predict hesitancy rates in more granular areas using the Census Bureau's 2019 American Community Survey (ACS) 1-year Public Use Microdata Sample (PUMS). To create county-level estimates, we used a PUMA-to-county crosswalk from the Missouri Census Data Center. PUMAs spanning multiple counties had their estimates apportioned across those counties based on overall 2010 Census populations."
+
 rawdata <- readxl::read_excel(data_location)
 
 #take a look at the data
 View(rawdata)
 
-#Filtering and selecting wanted Georgia data. The objective is to grab data for changes in proportion of vaccine hesitant and unsure with different demographic factors. 
+#Filtering and selecting wanted Georgia data. The objective is to grab data for changes in proportion of vaccine hesitant and unsure in the population (dependent variable) with different demographic factors.
+#The independent variables that I decided to look at were Social Vulnerability Index (SVI), CVAC Level Of Concern, Percent adults fully vaccinated against COVID-19 (as of 6/10/21).
 processeddata <- rawdata %>% 
   dplyr::select("State", "County Name", "Estimated hesitant", "Estimated hesitant or unsure", "Estimated strongly hesitant", "Social Vulnerability Index (SVI)", "CVAC Level Of Concern", "Percent adults fully vaccinated against COVID-19 (as of 6/10/21)")%>%
   dplyr::filter( State == "GEORGIA" )
